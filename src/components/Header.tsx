9385e8172660428888e7c48eb6b9a7acbe5ce05c
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-06 16:13:33 -0300

    fixing tailwind css features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,17 @@ interface Props {
 
 export default function Header({ userScore, machineScore }: Props) {
     return (
-        <header>
-            <div className="bg-black" text-white text-center p-5>
+        <>
+            <div className='bg-[#0E6BA8] text-white text-center p-5'>
                 <div>
                     <p className="text-[1rem] font-bold">Scoreboard</p>
                 </div>
-                <div className="flex lg:justify-center">
+                <div className='flex lg:justify-center '>
                     <div className="lg:w-1/5 w-1/3 lg:text-[2rem] text-[1.5rem] font-bold">User</div>
                     <div className="lg:w-1/5 w-1/3 lg:text-[2rem] text-[1.5rem] font-bold">{userScore} x {machineScore}</div>
                     <div className="lg:w-1/5 w-1/3 lg:text-[2rem] text-[1.5rem] font-bold">Machine</div>
                 </div>
             </div>
-        </header>
+        </>
     )
 }
